98999f6c30272d4acd952f5c8194857c36e0edab
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-29 13:57:15 -0500

    styles: added custom palette with primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primaryGreen: {
+          "100": "#a1d6b7",
+          "200": "#7ec79e",
+          "300": "#5bb884",
+          "400": "#38a96b",
+          "500": "#0f6935",
+          "600": "#0d5e30",
+          "700": "#0b532b",
+          "800": "#094826",
+          "900": "#073d21",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
