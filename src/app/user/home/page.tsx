2ff02dc4ddac19950fbff5d995c1e400928d9e1e
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-29 16:22:29 -0500

    feat: welcome section done.

---
+++ src/app/user/home/page.tsx
@@ -0,0 +1,47 @@
+import { StaticImageData } from "next/image"
+import {
+  UserIcon,
+  SquaresPlusIcon,
+  ArrowPathIcon,
+} from "@heroicons/react/24/outline"
+import Card from "@/components/Card"
+
+export type option = {
+  id?: number
+  icon?: any
+  title?: string
+}
+
+const options: Array<option> = [
+  {
+    id: 1,
+    icon: <UserIcon />,
+    title: "Perfiles",
+  },
+  {
+    id: 2,
+    icon: <SquaresPlusIcon />,
+    title: "Ingreso de datos",
+  },
+  {
+    id: 3,
+    icon: <ArrowPathIcon />,
+    title: "Actualizacion de datos",
+  },
+]
+
+function Home() {
+  return (
+    <div className="w-full h-[calc(100vh-4rem)] flex flex-col justify-center items-center gap-28">
+      <header className="text-center ml-44">
+        <h1 className="text-5xl">Bienvenido a la base de datos de Coomofu</h1>
+      </header>
+      <main className="ml-44 flex justify-center items-center flex-col md:flex-row gap-12">
+        {options.map((option: option) => {
+          return <Card key={option.id} {...option} />
+        })}
+      </main>
+    </div>
+  )
+}
+export default Home
---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import { option } from "@/app/user/home/page"
+import Link from "next/link"
+
+function Card({ icon, title }: option) {
+  return (
+    <Link
+      href={"/"}
+      className="w-48 h-48 lg:max-w-xs bg-primaryGreen-500 text-slate-100 hover:scale-105 rounded-md hover:text-secondaryYellow transition-all"
+    >
+      <div className="w-auto h-auto p-4">{icon}</div>
+      <h1 className="text-center text-lg pt-2 text-slate-800">{title}</h1>
+    </Link>
+  )
+}
+export default Card
