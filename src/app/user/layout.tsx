9c013b779697411e4f2ec36ee0f2a899ef30eaa2
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-29 19:05:29 -0500

    chore: implementation of the sidebar.

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,9 +1,20 @@
+"use client"
+
 import Sidebar from "@/components/Sidebar"
+import { useState } from "react"
 
 function Landinglayout({ children }: any) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
   return (
     <>
-      <Sidebar />
+      <button
+        className="bg-cyan-300"
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+      >
+        Open / Close
+      </button>
+      <Sidebar isOpen={isSidebarOpen} />
       {children}
     </>
   )
