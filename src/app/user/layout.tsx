9d2506fa694a9bf942c436851c998e6069e22df2
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-29 23:35:42 -0500

    fix: rendering of icons.

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,18 +1,28 @@
 "use client"
 
 import Sidebar from "@/components/Sidebar"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import { useState } from "react"
 
 function Landinglayout({ children }: any) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
+  const sidebarIconStyles: string = "w-16 lg:w-32 h-10 text-slate-100"
+
   return (
     <>
+      {isSidebarOpen || (
+        <div className="w-full h-full absolute top-0 left-0 bg-slate-950/70 "></div>
+      )}
       <button
-        className="bg-cyan-300"
+        className="bg-primaryGreen-500 relative w-16 lg:w-32 h-16 z-10"
         onClick={() => setIsSidebarOpen(!isSidebarOpen)}
       >
-        Open / Close
+        {isSidebarOpen ? (
+          <Bars3Icon className={sidebarIconStyles} />
+        ) : (
+          <XMarkIcon className={sidebarIconStyles} />
+        )}
       </button>
       <Sidebar isOpen={isSidebarOpen} />
       {children}
