eeaf0f7d83594560e2f52fc981f21d0aef8098fc
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-29 19:07:10 -0500

    feat: dynamic routing.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,10 @@
 import { option } from "@/app/user/home/page"
 import Link from "next/link"
 
-function Card({ icon, title }: option) {
+function Card({ icon, title, route }: option) {
   return (
     <Link
-      href={"/"}
+      href={`${route}`}
       className="w-48 h-48 lg:max-w-xs bg-primaryGreen-500 text-slate-100 hover:scale-105 rounded-md hover:text-secondaryYellow transition-all"
     >
       <div className="w-auto h-auto p-4">{icon}</div>
