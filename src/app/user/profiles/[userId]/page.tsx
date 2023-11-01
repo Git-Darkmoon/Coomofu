0517afdfa28b824e18f9a575ddcbd1c0ea555ffb
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-31 22:51:26 -0500

    feat: import of the <Inputs/> component.

--- src/app/user/profiles/[userId]/page.tsx
+++ src/app/user/profiles/[userId]/page.tsx
@@ -2,6 +2,7 @@ import { fetchUsers } from "@/app/utils"
 import Image from "next/image"
 import Link from "next/link"
 import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded"
+import Inputs from "@/components/Inputs"
 
 const API_URL = "https://reqres.in/api/users"
 
@@ -36,116 +37,7 @@ async function page({ params }: any) {
         </div>
       </article>
 
-      <article>
-        <div className="grid gap-6 mb-6 md:grid-cols-2 ">
-          <div>
-            <label
-              htmlFor="first_name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Nombre
-            </label>
-            <input
-              type="text"
-              id="first_name"
-              className="bg-gray-50 border cursor-not-allowed border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder={`${first_name} ${last_name}`}
-              readOnly
-              required
-            />
-          </div>
-
-          <div>
-            <label
-              htmlFor="company"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Cedula
-            </label>
-            <input
-              type="text"
-              id="company"
-              className="bg-gray-50 border cursor-not-allowed border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder={crypto.randomUUID().slice(0, 8)}
-              readOnly
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="last_name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Telefono
-            </label>
-            <input
-              type="tel"
-              id="last_name"
-              className="bg-gray-50 border cursor-not-allowed border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="+57 309 387 6691"
-              readOnly
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="phone"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Fecha de Nacimiento
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              className="bg-gray-50 border cursor-not-allowed border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="21/04/1998"
-              pattern="[0-9]{2}/[0-9]{2}/[0-9]{4}"
-              readOnly
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="website"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Direccion
-            </label>
-            <input
-              type="text"
-              id="website"
-              className="bg-gray-50 border cursor-not-allowed border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Cll 97 #4-05 apartamento 901"
-              readOnly
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="visitors"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="visitors"
-              className="bg-gray-50 border cursor-not-allowed border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder={email}
-              readOnly
-              required
-            />
-          </div>
-          <div className="flex gap-3">
-            <button className="text-slate-100 px-1.5 py-1 rounded-sm hover:text-secondaryYellow bg-primaryGreen-400">
-              Editar Informacion
-            </button>
-            <button className="text-slate-100 px-1.5 py-1 rounded-sm hover:text-secondaryYellow bg-red-500">
-              Borrar Usuario
-            </button>
-          </div>
-        </div>
-      </article>
+      <Inputs first_name={first_name} last_name={last_name} email={email} />
     </main>
   )
 }
