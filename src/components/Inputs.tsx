300d084f9258f772440a02b035a0b7a98787df91
Nicolas Mayorga <namayorgav@gmail.com>
2023-11-05 17:15:50 -0500

    feat: Swal implementation and states.

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,14 +1,17 @@
 "use client"
 
+import { useRouter } from "next/navigation"
 import { useState } from "react"
+import Swal from "sweetalert2"
 
-function Inputs({ first_name, last_name, email }: any) {
-  const [isReadOnly, setIsReadOnly] = useState(true)
+function Inputs({ first_name, last_name, email, initialState }: any) {
+  const [isReadOnly, setIsReadOnly] = useState<boolean>(initialState)
+  const router = useRouter()
 
   const inputStyles: string = `bg-gray-50 border ${
     isReadOnly
       ? "cursor-not-allowed outline-none"
-      : "cursor-default border-gray-300"
+      : "cursor-text border-gray-300"
   }  text-gray-900 text-sm rounded-md block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white `
 
   return (
@@ -19,7 +22,7 @@ function Inputs({ first_name, last_name, email }: any) {
             htmlFor="first_name"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Nombre
+            Primer Nombre y Primer Apellido
           </label>
           <input
             type="text"
@@ -43,7 +46,7 @@ function Inputs({ first_name, last_name, email }: any) {
             type="text"
             id="company"
             className={inputStyles}
-            placeholder={crypto.randomUUID().slice(0, 8)}
+            placeholder={`${Math.round(Math.random() * 1000000) * 100000}`}
             readOnly={isReadOnly}
             autoComplete="off"
             required
@@ -68,16 +71,15 @@ function Inputs({ first_name, last_name, email }: any) {
         </div>
         <div>
           <label
-            htmlFor="phone"
+            htmlFor="birth_date"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Fecha de Nacimiento
           </label>
           <input
-            type="tel"
-            id="phone"
+            type="date"
+            id="birth_date"
             className={inputStyles}
-            placeholder="21/04/1998"
             pattern="[0-9]{2}/[0-9]{2}/[0-9]{4}"
             readOnly={isReadOnly}
             autoComplete="off"
@@ -120,19 +122,57 @@ function Inputs({ first_name, last_name, email }: any) {
         </div>
         <div className="flex gap-3">
           <button
-            className="text-slate-100 rounded-md px-2 py-3 hover:text-secondaryYellow hover:bg-primaryGreen-600 bg-primaryGreen-400 transition-colors"
+            className="text-slate-100 rounded-md px-12 py-3 hover:text-secondaryYellow hover:bg-primaryGreen-600 bg-primaryGreen-400 transition-colors"
             onClick={() => {
-              isReadOnly
-                ? alert("Estas en modo edicion")
-                : alert("Datos guardados correctamente")
+              if (isReadOnly) {
+              } else {
+                Swal.fire({
+                  title: "Quieres guardar los datos?",
+                  showDenyButton: true,
+                  confirmButtonColor: "#38a96b",
+                  confirmButtonText: "Si, guardar",
+                  denyButtonText: `No, aun no`,
+                }).then((result) => {
+                  /* Read more about isConfirmed, isDenied below */
+                  if (result.isConfirmed) {
+                    Swal.fire("Datos guardados correctamente!", "", "success")
+                    router.push("/user/home")
+                  } else if (result.isDenied) {
+                    Swal.fire(
+                      "Dale click en 'Editar Informacion' si deseas cambiar los datos.",
+                      "",
+                      "info"
+                    )
+                  }
+                })
+              }
 
               setIsReadOnly(!isReadOnly)
             }}
           >
-            {isReadOnly ? "Editar Informacion" : "Guardar Cambios"}
+            {isReadOnly ? "Editar Informacion" : "Guardar"}
           </button>
           <button
-            onClick={() => confirm("Estas seguro de borrar a ese usuario ?")}
+            onClick={() =>
+              Swal.fire({
+                title: `Estas seguro de eliminar a ${first_name}?`,
+                text: "No se podra revertir esta accion.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#38a96b",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Si, eliminar!",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  Swal.fire(
+                    "Eliminado!",
+                    `${first_name} ha sido eliminado.`,
+                    "success"
+                  )
+                }
+                router.push("/user/home")
+              })
+            }
             className={`text-slate-100 rounded-md px-2 py-3 hover:text-secondaryYellow hover:bg-red-700 bg-red-500 transition-colors ${
               isReadOnly ? "visible" : "invisible"
             }`}
